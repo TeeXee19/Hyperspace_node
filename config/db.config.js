0ef5a6b9987b03d311a74dd51bae3f031777e177
Tochukwu Franklin Ene <teec3000@gmail.com>
2021-03-30 11:23:33 +0100

    Added the heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const mysql = require('mysql');
-//local mysql db connection
+// //local mysql db connection
+// const dbConn = mysql.createConnection({  
+// 	host     : 'localhost',  
+// 	user     : 'root', 
+// 	password : '',  
+// 	database : 'hyperspace_db'
+// });
+
+
+//Heroku mysql db connection
 const dbConn = mysql.createConnection({  
-	host     : 'localhost',  
-	user     : 'root', 
-	password : '',  
-	database : 'hyperspace_db'
+	host     : 'us-cdbr-east-03.cleardb.com',  
+	user     : 'b5251d36042f2b', 
+	password : '835addfc',  
+	database : 'heroku_66b8eafc1af4aef'
 });
+
+
 
 dbConn.connect(
 	function(err) {  
