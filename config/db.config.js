d517dc2b1a5b8e58aada77133af4d248a9a1f206
Tochukwu Franklin Ene <teec3000@gmail.com>
2021-03-30 16:05:53 +0100

    Changes db connection

--- config/db.config.js
+++ config/db.config.js
@@ -10,8 +10,8 @@ const mysql = require('mysql');
 // });
 
 
-//Heroku mysql db connection
-const dbConn = mysql.createConnection({  
+// Heroku mysql db connection
+const dbConn = mysql.createPool({  
 	host     : 'us-cdbr-east-03.cleardb.com',  
 	user     : 'b5251d36042f2b', 
 	password : '835addfc',  
@@ -20,10 +20,23 @@ const dbConn = mysql.createConnection({
 
 
 
-dbConn.connect(
-	function(err) {  
-		if (err) throw err;  
-		console.log("Database Connected!");
-	});
+// dbConn.connect(
+// 	function(err) {  
+// 		if (err) throw err;  
+// 		console.log("Database Connected!");
+// 	});
+
+ // dbConn.on('error', function(err) {
+ //    console.log('db error', err);
+ //    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+	//    dbConn.connect(
+	// 		function(err) {  
+	// 			if (err) throw err;  
+	// 			console.log("Database Connected!");
+	// 		});                         // lost due to either server restart, or a
+ //    } else {                                      // connnection idle timeout (the wait_timeout
+ //      throw err;                                  // server variable configures this)
+ //    }
+ //  });
 
 module.exports = dbConn;
